a3324e97cda24cd1027ed389acd05353baddf35c
tiago-ribeiro-sharkcoders <158041616+tiago-ribeiro-sharkcoders@users.noreply.github.com>
2024-03-23 10:31:48 +0000

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,14 @@
+class Curropecao:
+
+    def __init__(self) ->None:
+        self.__ordenado = 10.000
+        print("ordenado")
+    
+    def get_ordenado(self):
+        return self.__ordenado
+    
+    def set_ordenado(self, ordenado):
+        self.__ordenado = ordenado
+    
+
+c1 = Curropecao()
